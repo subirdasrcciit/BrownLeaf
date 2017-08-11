dd2b9b1a7a8d98beea19a8ecdcfdd397fe0ac531
subirdasrcciit <subirdasrcciit@gmail.com>
2017-08-11 14:11:55 +0530

    Azure SDK Update

--- app/src/main/java/com/app/restaurant/brownleaf/BrownLeafApp.java
+++ app/src/main/java/com/app/restaurant/brownleaf/BrownLeafApp.java
@@ -3,6 +3,11 @@ package com.app.restaurant.brownleaf;
 import android.app.Application;
 import android.content.Context;
 
+import com.app.restaurant.brownleaf.util.Constants;
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+
 /**
  * Created by mobilecoe-imac6 on 12/01/17.
  */
@@ -15,6 +20,9 @@ public class BrownLeafApp extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = this;
+
+        MobileCenter.start(this, Constants.MOBILE_CENTER_KEY,
+                Analytics.class, Crashes.class);
     }
 
     public static Context getContext(){
